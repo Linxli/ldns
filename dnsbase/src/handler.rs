5401430381a5f2c19712603b833f430558905e36
ff <ff@Winbreaker.localdomain>
2025-10-19 16:57:13 +0200

    new addons, needed to push bc wsl is shit

--- dnsbase/src/handler.rs
+++ dnsbase/src/handler.rs
@@ -17,6 +17,46 @@ pub struct Handler {
 }
 
 impl Handler {
+    // /// Create new handler from command-line options.
+    // pub fn from_options(_options: &Options) -> Self {
+    //     Handler {}
+    // }
+
+    /// Handle requests for counter.{domain}.
+    async fn do_handle_request_counter<R: ResponseHandler>(
+        &self,
+        request: &Request,
+        mut responder: R,
+    ) -> Result<ResponseInfo, Error> {
+        let counter = self.counter.fetch_add(1, Ordering::SeqCst);
+        let builder = MessageResponseBuilder::from_message_request(request);
+        let mut header = Header::response_from_request(request.header());
+        header.set_authoritative(true);
+        let rdata = RData::TXT(TXT::new(vec![counter.to_string()]));
+        let records = vec![Record::from_rdata(request.query().name().into(), 60, rdata)];
+        let response = builder.build(header, records.iter(), &[], &[], &[]);
+        Ok(responder.send_response(response).await?)
+    }
+
+    /// Handle requests for myip.{domain}.
+    async fn do_handle_request_myip<R: ResponseHandler>(
+        &self,
+        request: &Request,
+        mut responder: R,
+    ) -> Result<ResponseInfo, Error> {
+        self.counter.fetch_add(1, Ordering::SeqCst);
+        let builder = MessageResponseBuilder::from_message_request(request);
+        let mut header = Header::response_from_request(request.header());
+        header.set_authoritative(true);
+        let rdata = match request.src().ip() {
+            IpAddr::V4(ipv4) => RData::A(ipv4),
+            IpAddr::V6(ipv6) => RData::AAAA(ipv6),
+        };
+        let records = vec![Record::from_rdata(request.query().name().into(), 60, rdata)];
+        let response = builder.build(header, records.iter(), &[], &[], &[]);
+        Ok(responder.send_response(response).await?)
+    }
+
     /// Handle request, returning ResponseInfo if response was successfully sent, or an error.
     async fn do_handle_request<R: ResponseHandler>(
         &self,
@@ -48,6 +88,31 @@ impl Handler {
             }
             name => Err(Error::InvalidZone(name.clone())),
         }
+    }
+
+    /// Handle requests for *.hello.{domain}.
+    async fn do_handle_request_hello<R: ResponseHandler>(
+        &self,
+        request: &Request,
+        mut responder: R,
+    ) -> Result<ResponseInfo, Error> {
+        self.counter.fetch_add(1, Ordering::SeqCst);
+        let builder = MessageResponseBuilder::from_message_request(request);
+        let mut header = Header::response_from_request(request.header());
+        header.set_authoritative(true);
+        let name: &Name = request.query().name().borrow();
+        let zone_parts = (name.num_labels() - self.hello_zone.num_labels() - 1) as usize;
+        let name = name
+            .iter()
+            .enumerate()
+            .filter(|(i, _)| i <= &zone_parts)
+            .fold(String::from("hello,"), |a, (_, b)| {
+                a + " " + &String::from_utf8_lossy(b)
+            });
+        let rdata = RData::TXT(TXT::new(vec![name]));
+        let records = vec![Record::from_rdata(request.query().name().into(), 60, rdata)];
+        let response = builder.build(header, records.iter(), &[], &[], &[]);
+        Ok(responder.send_response(response).await?)
     }
 }
 
