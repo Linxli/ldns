3b06d7f9ad1244c358bf458662846be9df9badf5
Linxli <fabio.frick@protonmail.com>
2025-11-01 09:09:56 +0100

    check curl

--- dnsraw/src/blocklookup.rs
+++ dnsraw/src/blocklookup.rs
@@ -1,7 +1,7 @@
+use curl::easy::Easy;
 use hickory_proto::rr::domain::Name;
-use reqwest::{Url, get};
 use std::fs::read_to_string;
-use tokio::fs::write;
+use std::io::{Write, stdout};
 use tokio::time;
 
 const DNS_LIST: &str = "/Users/fabio/ldns/dnsblock.txt";
@@ -27,30 +27,48 @@ pub fn check_dn_block_list(qname: Name) -> bool {
 }
 
 pub async fn check_blocklist_update(time: u64) {
+    println!("we are in check update");
     let web_addr =
         "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/adblock/ultimate.txt";
 
     loop {
-        match get(web_addr).await {
-            Ok(resp) => {
-                if resp.status().is_success() {
-                    match resp.text().await {
-                        Ok(body) => {
-                            if let Err(e) = write(DNS_LIST, body.clone()).await {
-                                eprintln!("Error writing to file: {}", e);
-                            } else {
-                                println!("blocklist has been updated!")
-                            }
-                        }
-                        Err(e) => eprint!("error parsing responese to text: {}", e),
-                    }
-                } else {
-                    eprint!("Failed to access the websit, no 200: {}", resp.status());
-                }
-            }
-            Err(e) => eprint!("Failed to fetch blocklist: {}", e),
-        };
-
+        println!("we loop");
+        let mut curl = Easy::new();
+        curl.url(web_addr);
+        println!("curl done");
+        let data = curl
+            .write_function(|data| {
+                stdout().write_all(data);
+                Ok(data.len())
+            })
+            .unwrap();
+        println!("parsing dingis done");
+        // match data {
+        //     Ok(_) => _,
+        //     Err(e) => eprintln!("An Error occourd: {}", e),
+        // };
+        println!("DATA DOWN: {:?}", data);
         time::sleep(time::Duration::from_secs(time * 3600)).await;
     }
 }
+
+// match get(web_addr).await {
+//     Ok(resp) => {
+//         if resp.status().is_success() {
+//             match resp.text().await {
+//                 Ok(body) => {
+//                     if let Err(e) = write(DNS_LIST, body.clone()).await {
+//                         eprintln!("Error writing to file: {}", e);
+//                     } else {
+//                         println!("blocklist has been updated!")
+//                     }
+//                 }
+//                 Err(e) => eprint!("error parsing responese to text: {}", e),
+//             }
+//         } else {
+//             eprint!("Failed to access the websit, no 200: {}", resp.status());
+//         }
+//     }
+//     Err(e) => eprint!("Failed to fetch blocklist: {}", e),
+// };
+//
